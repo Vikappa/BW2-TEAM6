893fdf506680c119496349d06ac5b278489cc951
Vikappa <vincenzo.e.costantini@gmail.com>
2024-01-22 23:10:17 +0100

    Update

--- scripts/index.js
+++ scripts/index.js
@@ -4,16 +4,28 @@ import { searchAlbum } from './searchAlbumQuery.js'
 
 
 // UTILITY
+
+const updatePlayBar = function () {
+    const playerControls = document.createElement('div')
+    const backIcon = document.createElement('i')
+    const nextIcon = document.createElement('i')
+    const playIcon = document.createElement('i')
+
+    playIcon.innerHTML = `<i class="bi bi-play-circle-fill text-primary p-0 m-0" style="font-size:4.5rem;"></i>`
+    backIcon.innerHTML = `<i class="bi bi-play-circle-fill text-primary p-0 m-0" style="font-size:4.5rem;"></i>`
+    nextIcon.innerHTML = `<i class="bi bi-skip-forward-fill text-primary p-0 m-0" style="font-size:4.5rem;"></i>`
+    document.getElementById('playBar').appendChild(playerControls)
+}
+
 const start = function () {
     searchAlbum("Post Humans Survival Horror").then(arrayRitorno => {
-        console.log(arrayRitorno[0])
         document.getElementById('titoloHeaderTrack').textContent = arrayRitorno[0].title
         document.getElementById('artistHeaderTrack').textContent = arrayRitorno[0].artist.name
         document.getElementById('ascolta').textContent = arrayRitorno[0].artist.name
         document.getElementById('recordType').textContent = arrayRitorno[0].album.type
         document.getElementById('imgHeroTrack').src = arrayRitorno[0].album.cover_medium
 
-
+        updatePlayBar(arrayRitorno[0].preview)
 
     }).catch(err => {
         console.error(err)
