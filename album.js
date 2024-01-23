f3bd6258d3a7b21cfe483874ba61d0bbde2eb326
Vikappa <vincenzo.e.costantini@gmail.com>
2024-01-23 13:05:57 +0100

    Update album JS

--- album.js
+++ album.js
@@ -1,10 +1,5 @@
-// REFERENCES
+import { catchAlbum } from './scripts/fetchAlbumObg.js'
 
-const albumAPI =
-  "https://striveschool-api.herokuapp.com/api/deezer/album/75621062";
-
-// const addressBarContent = new URLSearchParams(location.search);
-// const albumId = addressBarContent.get("albumId");
 
 const albumCover = document.getElementById("albumCover");
 const albumTracks = document.getElementById("albumTracks");
@@ -13,16 +8,11 @@ const trackReproductions = document.getElementById("trackReproductions");
 const trackDuration = document.getElementById("trackDuration");
 
 // FETCH
-fetch(albumAPI)
-  .then((response) => {
-    if (response.ok) {
-      console.log(response);
-      return response.json();
-    }
-  })
-  .then((album) => {
-    console.log(album);
-    // POPOLAMENTO DINAMICO DELLA SEZIONE ALBUM (copertina e info)
+let album
+catchAlbum('75621062')
+  .then(result => {
+    album = result
+
     const newRow = document.createElement("div");
     newRow.classList.add("row");
     newRow.innerHTML = `
@@ -57,44 +47,44 @@ fetch(albumAPI)
       )}</p>
   </div>`;
     albumCover.appendChild(newRow);
-    // POPOLAMENTO DINAMICO DELLE TRACCE DELL'ALBUM
-    album.tracks.data.forEach((element, i) => {
+    console.log(album.tracks)
+    for (let i = 0; i < album.tracks.length; i++) {
+      const element = album.tracks[i];
+
       const newRowDesktop = document.createElement("div");
-      newRowDesktop.classList.add("row");
-      newRowDesktop.classList.add("d-none");
-      newRowDesktop.classList.add("d-md-flex");
+      newRowDesktop.classList.add("row", "d-none", "d-md-flex");
       newRowDesktop.innerHTML = `
-      <div class="col-7 d-flex mb-3">
-      <p class="me-4 mb-0 text-secondary d-flex align-items-center">${i + 1}</p>
-      <div class="d-flex flex-column">
-      <p class="text-white mb-1">${element.title}</p>
-      <p class="m-0 text-secondary">${element.artist.name}
-      </p>
-      </div>
-    </div>
-    <div class="col-3 d-flex align-items-center text-secondary mb-3">
-      <p class="mb-0">${element.rank}</p>
-    </div>
-    <div class="col-2 text-center d-flex align-items-center justify-content-center text-secondary mb-3">
-    <p class="mb-0">${Math.floor(element.duration / 60)}:${element.duration % 60
-        }</p>
-    </div>`;
+          <div class="col-7 d-flex mb-3">
+              <p class="me-4 mb-0 text-secondary d-flex align-items-center">${i + 1}</p>
+              <div class="d-flex flex-column">
+                  <p class="text-white mb-1">${element.title}</p>
+                  <p class="m-0 text-secondary">${element.artist.name}</p>
+              </div>
+          </div>
+          <div class="col-3 d-flex align-items-center text-secondary mb-3">
+              <p class="mb-0">${element.rank}</p>
+          </div>
+          <div class="col-2 text-center d-flex align-items-center justify-content-center text-secondary mb-3">
+              <p class="mb-0">${Math.floor(element.duration / 60)}:${element.duration % 60}</p>
+          </div>`;
       albumTracks.appendChild(newRowDesktop);
+
       const newRowMobile = document.createElement("div");
-      newRowMobile.classList.add("row");
-      newRowMobile.classList.add("d-md-none");
+      newRowMobile.classList.add("row", "d-md-none");
       newRowMobile.innerHTML = `
-      <div class="col-12 d-flex justify-content-between align-items-center mb-3">
-        <div>
-          <p class="text-white mb-0">${element.title_short}</p>
-          <p class="text-secondary mb-0">${element.artist.name}</p>
-        </div>
-        <button class="btn btn-black text-secondary fs-3"><i class="bi bi-three-dots-vertical"></i></button>
-      </div>
-      `;
+          <div class="col-12 d-flex justify-content-between align-items-center mb-3">
+              <div>
+                  <p class="text-white mb-0">${element.title_short}</p>
+                  <p class="text-secondary mb-0">${element.artist.name}</p>
+              </div>
+              <button class="btn btn-black text-secondary fs-3"><i class="bi bi-three-dots-vertical"></i></button>
+          </div>`;
       albumTracks.appendChild(newRowMobile);
-    });
+    }
+
   })
-  .catch((err) => {
-    alert("Ops... Something went wrong", err);
-  });
+  // POPOLAMENTO DINAMICO DELLE TRACCE DELL'ALBUM
+
+  .catch(error => {
+    console.error('Si è verificato un errore nella casella di ricerca:', error)
+  })
