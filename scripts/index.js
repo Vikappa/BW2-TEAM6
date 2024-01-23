7860ad4e6671d2c21f3e5f72b5d3a1df0bbbf7c4
Vikappa <vincenzo.e.costantini@gmail.com>
2024-01-23 01:18:34 +0100

    Update Vincenzo's listening

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,8 @@ import { searchAlbum } from './searchAlbumQuery.js'
 // UTILITY
 
 const updatePlayBar = function (trackObj) {
+    console.log('prova')
+    document.getElementById('playBar').innerHTML = ``
     document.getElementById('playBar').appendChild(audioPlayer(trackObj))
 }
 
@@ -26,3 +28,17 @@ const start = function () {
 
 // INIZIO ESECUZIONE VERA E PROPRIA
 start()
+
+document.getElementById("Vincenzos").addEventListener('click', () => {
+    searchAlbum("Mantra Bring Me The Horizon").then(arrayRitorno => {
+        document.getElementById('titoloHeaderTrack').textContent = arrayRitorno[0].title
+        document.getElementById('artistHeaderTrack').textContent = arrayRitorno[0].artist.name
+        document.getElementById('ascolta').textContent = arrayRitorno[0].artist.name
+        document.getElementById('recordType').textContent = arrayRitorno[0].album.type
+        document.getElementById('imgHeroTrack').src = arrayRitorno[0].album.cover_medium
+        updatePlayBar(arrayRitorno[0])
+
+    }).catch(err => {
+        console.error(err)
+    })
+})
