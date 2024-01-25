fdef821a819a728ca09d4e1070e80f34a3c04fae
Vikappa <vincenzo.e.costantini@gmail.com>
2024-01-25 03:05:55 +0100

    Create userClass.js

---
+++ scripts/userClass.js
@@ -0,0 +1,9 @@
+class User {
+    constructor(nome, arrayArtisti, arrayPiaciute, arrayUltimeRicerche, arrayUltimiAlbum) {
+        this.nome = nome
+        this.arrayArtisti = arrayArtisti
+        this.arrayPiaciute = arrayPiaciute
+        this.arrayUltimeRicerche = arrayUltimeRicerche
+        this.arrayUltimiAlbum = arrayUltimiAlbum
+    }
+}
