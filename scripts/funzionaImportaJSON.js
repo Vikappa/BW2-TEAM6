f6c363ac4b4d66e6bdc647f342ad3a6129b64fd2
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-01-28 05:44:39 +0100

    Revert "Update directory dinamica github pages"
    This reverts commit 5ed641dd27a9a3c942a483879bcdbdbd703f07e7.

--- scripts/funzionaImportaJSON.js
+++ scripts/funzionaImportaJSON.js
@@ -1,18 +1,16 @@
 import { PlayList } from './playListClass.js'
 
 export function fetchJSON() {
-    const baseUrl = window.location.hostname.includes('github.io') ? '/nome-repo/' : '/'
-
-    return fetch(baseUrl + 'playlistPreconfezionate/suggerimenti_premade.json')
+    return fetch('../playlistPreconfezionate/suggerimenti_premade.json')
         .then(response => response.json())
         .then(data => {
-            let arrayPlayListRitorno = [];
+            let arrayPlayListRitorno = []
             for (let index = 0; index < data.arrayPlayListPreconfezionate.length; index++) {
                 let playListArtisti = new PlayList(
                     data.arrayPlayListPreconfezionate[index].nome,
                     data.arrayPlayListPreconfezionate[index].arrayTrack,
                     data.arrayPlayListPreconfezionate[index].img
-                );
+                )
                 arrayPlayListRitorno.push(playListArtisti)
             }
             return arrayPlayListRitorno
