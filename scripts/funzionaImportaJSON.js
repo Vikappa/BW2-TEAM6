0452eef07183549725e2035ff5037c345a966c2f
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-01-28 10:33:17 +0100

    Update url dinamico fetch

--- scripts/funzionaImportaJSON.js
+++ scripts/funzionaImportaJSON.js
@@ -1,9 +1,9 @@
 import { PlayList } from './playListClass.js'
 
 export function fetchJSON() {
-    const baseUrl = window.location.hostname.includes('github.io') ? '/nome-repo/' : '/'
+    const baseUrl = window.location.hostname.includes('github.io') ? '/BW2-TEAM6/' : '/'
 
-    return fetch('https://github.com/Vikappa/BW2-TEAM6/blob/main/playlistPreconfezionate/suggerimenti_premade.json')
+    return fetch(baseUrl + 'playlistPreconfezionate/suggerimenti_premade.json')
         .then(response => response.json())
         .then(data => {
             let arrayPlayListRitorno = [];
